bb5ae5b6c22c2ac417021d2847402658b7e53989
johns <johnsun2145@outlook.com>
2024-05-30 23:41:28 +0800

    使用SpringBoot初始化项目并导入到IDEA

---
+++ springboot-case-001/src/main/java/pers/johns/springboot/SpringbootCase001Application.java
@@ -0,0 +1,13 @@
+package pers.johns.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCase001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCase001Application.class, args);
+	}
+
+}
