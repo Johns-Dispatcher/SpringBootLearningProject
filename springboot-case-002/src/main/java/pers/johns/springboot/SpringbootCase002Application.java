0d136651f77aabca8e1bf4e86e15e34054f7ccb8
johns <johnsun2145@outlook.com>
2024-05-31 09:18:31 +0800

    使用IDEA内置脚手架创建SpringBoot项目（未选择额外依赖）

---
+++ springboot-case-002/src/main/java/pers/johns/springboot/SpringbootCase002Application.java
@@ -0,0 +1,13 @@
+package pers.johns.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCase002Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCase002Application.class, args);
+	}
+
+}
