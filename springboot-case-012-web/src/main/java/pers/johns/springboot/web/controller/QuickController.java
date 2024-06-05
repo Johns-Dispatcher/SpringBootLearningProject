b46ad8a45a1288f318e4c5d574141cbadbfe71d7
johns <johnsun2145@outlook.com>
2024-06-06 00:33:21 +0800

    使用SpringBoot快速搭建Web项目

--- springboot-case-012-web/src/main/java/pers/johns/springboot/web/controller/QuickController.java
+++ springboot-case-012-web/src/main/java/pers/johns/springboot/web/controller/QuickController.java
@@ -2,11 +2,7 @@ package pers.johns.springboot.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.Random;
 
 /**
  * ClassName    : QuickController
